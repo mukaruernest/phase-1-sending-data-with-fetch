16fb67179f9d1e1fc12ce67db2f420d692778117
mukaruernest <mukaruernesto@gmail.com>
2023-01-23 07:27:20 +0300

    sending data with fetch assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    const data = { name: name, email: email };
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+        body: JSON.stringify(data)
+    };
+    return fetch('http://localhost:3000/users', options)
+        .then(response => response.json())
+        .then(data => {
+            const newId = data.id;
+            //Append newId to the DOM
+            const element = document.createElement("P");
+            const text = document.createTextNode("New User ID: " + newId);
+            element.appendChild(text);
+            document.body.appendChild(element);
+        })
+        .catch(error => {
+            //Append error message to the DOM
+            const element = document.createElement("P");
+            const text = document.createTextNode("Error: " + error.message);
+            element.appendChild(text);
+            document.body.appendChild(element);
+        });
+}
